557e16c154cfff1f4fc5dd213cfa8c1c24c2144b
michaelRS2002 <148493262+michaelRS2002@users.noreply.github.com>
2024-11-28 23:37:38 -0500

    use codeshares for connect with front

--- unizone/settings.py
+++ unizone/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework', #--> Nuevo
     'rest_framework_simplejwt', #--> Nuevo
     'apps.users',
