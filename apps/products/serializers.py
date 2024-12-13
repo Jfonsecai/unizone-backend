240c25de5dc639e81112184c6b925f54a695a3f2
michaelRS2002 <ramirez.michael@correounivalle.edu.co>
2024-12-12 20:43:27 -0500

    Update serializers.py

--- apps/products/serializers.py
+++ apps/products/serializers.py
@@ -5,3 +5,8 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ['id', 'name', 'description', 'price', 'available', 'photo']
+
+    def validate_name(self, value):
+        if ";" in value or "--" in value or "'" in value:
+            raise serializers.ValidationError("Input contains invalid characters.")
+        return value
