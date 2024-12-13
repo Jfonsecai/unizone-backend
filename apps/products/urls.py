b635a114549f7b6e61fc8b4acdbd7250c571eaae
Jfonsecai <juandavidfon@gmail.com>
2024-12-12 21:28:10 -0500

    Configured path for purchases

--- apps/products/urls.py
+++ apps/products/urls.py
@@ -1,8 +1,10 @@
 # urls.py
 from django.urls import path
 from . import views
+from .views import SimulatePurchaseView
 
 urlpatterns = [
     path('api/products/', views.ProductListCreate.as_view(), name='product-list-create'),
     path('products/<int:pk>/', views.ProductRetrieveUpdateDestroy.as_view(), name='product-detail'),
+    path('api/simulate-purchase/', SimulatePurchaseView.as_view(), name='simular-compra'),
 ]
